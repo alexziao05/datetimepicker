5ac47cb39f00234ddea02fa38c666a2dbaf5435a
Alex <alexziao05@gmail.com>
2025-09-05 19:02:19 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
+
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B", dtype="auto", device_map="auto")
+
